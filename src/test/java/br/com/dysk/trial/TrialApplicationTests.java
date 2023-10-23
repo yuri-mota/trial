f62bd6d161e7165a2485edf3c411f9e34ce28251
Yuri Mota <yuri.mota@outlook.com.br>
2023-10-22 23:21:03 -0300

    Initial commit

---
+++ src/test/java/br/com/dysk/trial/TrialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dysk.trial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
